f314cc7750774967701f1e7ace28bde96076dad9
Keshav0053 <132044925+Keshav0053@users.noreply.github.com>
2023-05-06 16:30:58 +0530

    Create check the whether a given number even or odd.cpp

---
+++ Assignment 2/check the whether a given number even or odd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter num.:";
+	cin>>n;
+	if(n%2==0){
+		cout<<n<<" "<<"even num.";
+	}
+	else{
+		cout<<n<<" "<<"odd num.";
+	}
+	return 0;
+}
