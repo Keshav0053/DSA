d542bd51ec1fe6685146726c4ed752934cc547a1
Keshav0053 <132044925+Keshav0053@users.noreply.github.com>
2023-05-09 23:05:21 +0530

    Update 02. Is student is allow to sit in exam or not.cpp

--- Assignment 2/02. Is student is allow to sit in exam or not.cpp
+++ Assignment 2/02. Is student is allow to sit in exam or not.cpp
@@ -1,19 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-float at,p,a;
-	 cout<<"Enter the number of total classes held:";
-	 cin>>a;
-	 cout<<"Enter the number of classes attended:";
-	 cin>>at;
+	float at,p,a,x=0;
+	char n;
+	cout<<"Enter the number of total classes held:";
+	cin>>a;
+	cout<<"Enter the number of classes attended:";
+	cin>>at;
 	p=at*100/a;
-if(p>=75){
+	if(p>=75){
+		x=1;
 		cout<<"Percentage of classes attended:"<<p<<"%"<<endl;
 		cout<<"Student is allowed to sit in exam."<<endl;
-}
-else{
+	}
+	else if(x==0){
 		cout<<"Percentage of classes attended:"<<p<<"%"<<endl;
+		cout<<"Student has medical cause or not .('Y' or 'N')"<<endl;
+		cout<<"Choose the option : ";
+		cin>>n;	
+		if(n=='Y'){
+		cout<<"Student is allowed to sit in exam."<<endl;	
+		}		
+		else{
 		cout<<"Student is not allowed to sit in exam."<<endl;
-}
+		}		
+	} 
 	return 0;
 }
