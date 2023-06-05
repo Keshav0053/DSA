3be11aeedebd74bb44d211f8da087ad658dff07e
Keshav0053 <132044925+Keshav0053@users.noreply.github.com>
2023-06-06 00:43:53 +0530

    Create 02. Sort 0 1 2.cpp

---
+++ Code Studio/02. Sort 0 1 2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //normal array
+   sort(arr,arr+n);
+   //sort(arr,arr+n,greatest<int>());
+}
+//vector
+//  sort(arr.begin(),end()
+//sort(arr.begin(),end(),greatest<int>());
