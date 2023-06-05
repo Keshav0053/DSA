5af94612dbac17589e5fb97de43e3d9c94f00d18
Keshav0053 <132044925+Keshav0053@users.noreply.github.com>
2023-06-06 00:24:36 +0530

    Create 03. Basic Data Types .cpp

---
+++ HackerRank c++/03. Basic Data Types .cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int i;
+long l;
+char ch;
+float f;
+double d;
+   cin>>i>>l>>ch>>f>>d;
+   printf("%d\n",i);
+   printf("%ld\n",l);
+   printf("%c\n",ch);
+   printf("%.3f\n",f);
+   printf("%.9lf\n",d);
+return 0;
+}
